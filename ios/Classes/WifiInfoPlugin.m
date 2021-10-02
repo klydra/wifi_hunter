5788381e7d0e718819a2ad003f2d5562c875f546
Jan Klinge <klydra@protonmail.com>
2021-10-02 03:04:41 +0200

    Removing old plugin version

--- ios/Classes/WifiInfoPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "WiFiHunterPlugin.h"
-#import <wifi_hunter/wifi_hunter-Swift.h>
-
-@implementation WiFiHunterPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftWiFiHunterPlugin registerWithRegistrar:registrar];
-}
-@end
