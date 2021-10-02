af574c8357a0537d139f4eddb61024affc58fd9c
Jan Klinge <klydra@protonmail.com>
2021-10-02 03:17:07 +0200

    Getting ready for republishing to pub.dev

--- lib/wifi_hunter_result.dart
+++ lib/wifi_hunter_result.dart
@@ -1,15 +1,15 @@
 class WiFiHunterResult {
-  late List<WiFiHunterResultEntry> results = <WiFiHunterResultEntry>[];
+  List<WiFiHunterResultEntry> results = <WiFiHunterResultEntry>[];
 }
 
 class WiFiHunterResultEntry {
-  late String SSID;
-  late String BSSID;
-  late String capabilities;
-  late int frequency;
-  late int level;
-  late int channelWidth;
-  late int timestamp;
+  String SSID;
+  String BSSID;
+  String capabilities;
+  int frequency;
+  int level;
+  int channelWidth;
+  int timestamp;
 
   WiFiHunterResultEntry(this.SSID, this.BSSID, this.capabilities, this.frequency, this.level, this.channelWidth, this.timestamp);
 }
